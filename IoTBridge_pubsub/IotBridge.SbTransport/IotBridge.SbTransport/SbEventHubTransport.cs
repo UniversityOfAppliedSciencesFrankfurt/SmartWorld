3b02e95ad98485785b13dc2ae6e07b5a7cfc5b95
Indraneel Pole <Indraneel.Pole@daenet.com>
2016-01-15 15:09:29 +0100

    IoTBridge over publish and subscribe message pattern

---
+++ IoTBridge_pubsub/IotBridge.SbTransport/IotBridge.SbTransport/SbEventHubTransport.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotBridge.SbTransport
+{
+    public class SbEventHubTransport : IBridgeTransport
+    {
+        public string Name
+        {
+            get { return "SbEventHubTransport"; }
+        }
+
+        public void OnMessage(Action<Message> onReceiveMsg, Dictionary<string, object> args = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message Receive(Dictionary<string, object> args = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendReceiveAckonwledgeResult(string msgId, Exception error, Dictionary<string, object> args = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(Message msg, Dictionary<string, object> args = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSendAcknowledgeResult(Action<string, Exception> onMsgSendResult, Dictionary<string, object> args = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
