757bed1b136afcd89cf3722cca72366e0a715e65
Damir Dobric <ddobric@daenet.de>
2016-03-19 11:08:11 +0100

    first ver.

---
+++ Connectors/IoTApi/IIoTTransport.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotBridge
+{
+    /// <summary>
+    /// Interface which defines operation which IoT transports have to implement.
+    /// </summary>
+    public interface IIoTTransport
+    {
+        /// <summary>
+        /// Performs initialization of transport implementation.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        Task Initialize(Dictionary<string, object> args);
+        
+        /// <summary>
+        /// Gets the name of transport library.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Invoked by transport when a message is received.
+        /// </summary>
+        /// <param name="onReceiveMsg">The method which is invoked when the message is received.</param>
+        /// <param name="args">List of arguments which can be internally used by transports.
+        /// Because transports will use different argumens
+        /// this parameter provides a generic dictionary of arguments.</param>
+        void OnMessage(Action<Message> onReceiveMsg, Dictionary<string, object> args = null);
+
+        /// <summary>
+        /// Receievs the message.
+        /// </summary>
+        /// <param name="args">List of arguments which can be internally used by transports.
+        /// Because transports will use different argumens
+        /// this parameter provides a generic dictionary of arguments.</param>
+        /// <returns></returns>
+        Message Receive(Dictionary<string, object> args = null);
+
+        /// <summary>
+        /// After the message is receieved the business code, whic h uses this transport,
+        /// MUST invoke this method to commit receiving of the message. 
+        /// If the transport does not support commit of the message, then implementation should 
+        /// return without any action.
+        /// </summary>
+        /// <param name="msgId">Identifier of the message, which should be commited.</param>
+        /// <param name="error">If no error ocurred this MUST BE null.
+        /// Otherwise this argument is instance of exception which has ocurred.</param>
+        /// <param name="args">List of arguments which can be internally used by transports.
+        /// Because transports will use different argumens
+        /// this parameter provides a generic dictionary of arguments.</param>
+        void SendReceiveAckonwledgeResult(string msgId, Exception error, Dictionary<string, object> args = null);
+
+        /// <summary>
+        /// Sends a message.
+        /// </summary>
+        /// <param name="msg">Message which will be sent.</param>
+        /// <param name="args">List of arguments which can be internally used by transports.
+        /// Because transports will use different argumens
+        /// this parameter provides a generic dictionary of arguments.</param>
+        void Send(Message msg, Dictionary<string, object> args = null);
+
+        /// <summary>
+        /// After sending of the message by using <see cref="Send"/> method, the transport will invoke
+        /// this method to notify business code about result of sending of the message.
+        /// If the transport does not support reliable messaging it should invoke this method without providing an exception.
+        /// First argument of type <see cref="string"/> specifyies the message identifier (used for correlation) and
+        /// second argument describes the error if some ocurred. If nmo error ocurred, second argument is null.
+        /// </summary>
+        /// <param name="onMsgSendResult"></param>
+        /// <param name="args">List of arguments which can be internally used by transports.
+        /// Because transports will use different argumens
+        /// this parameter provides a generic dictionary of arguments.</param>
+        void OnSendAcknowledgeResult(Action<string, Exception> onMsgSendResult, Dictionary<string, object> args = null);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg">Sends a message.</param>
+        /// <param name="onSuccess">On success delegate method (promise).</param>
+        /// <param name="onError">>On error delegate method (promise).</param>
+        /// <param name="args">List of arguments which can be internally used by transports.
+        /// Because transports will use different argumens
+        /// this parameter provides a generic dictionary of arguments.</param>
+        /// <returns></returns>
+        Task SendAsync(Message msg, Action<Message> onSuccess = null,
+          Action<Exception> onError = null,
+          Dictionary<string, object> args = null);
+    }
+}
---
+++ Connectors/IoTApi/Message.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotBridge
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+ 
+    
+    public class Message
+    {
+        // readonly MessagingClient client;
+        private object Body { get; set; }
+        private Stream BodyStream { get; set; }
+
+        //public string ContentType { get; set; }
+        //public string CorrelationId { get; set; }
+        //public int DeliveryCount { get; set; }
+        //public long EnqueuedSequenceNumber { get; set; }
+        //public DateTime EnqueuedTimeUtc { get; set; }
+        //public DateTime ExpiresAtUtc { get; internal set; }
+        //public bool IsBodyConsumed { get; set; }
+        //public string Label { get; set; }
+        //public DateTime LockedUntilUtc { get; internal set; }
+        //public Guid LockToken { get; internal set; }
+        public string MessageId { get; set; }
+        //public string PartitionKey { get; set; }
+        public Dictionary<string, object> Properties { get; set; }
+        //public string ReplyTo { get; set; }
+        //public string ReplyToSessionId { get; set; }
+        //public DateTime ScheduledEnqueueTimeUtc { get; set; }
+        //public long SequenceNumber { get; set; }
+        //public string SessionId { get; set; }
+        //public long Size { get; internal set; }
+  
+        //public TimeSpan TimeToLive { get; set; }
+        //public string To { get; set; }
+        //public string ViaPartitionKey { get; set; }
+
+        public Message()
+        {
+            initializeMessage();
+        }
+
+
+        /// <summary>
+        /// Creates the message.
+        /// </summary>
+        /// <param name="messageBody">The body of the message.</param>
+        public Message(object messageBody)
+        {
+            initializeMessage();
+            this.Body = messageBody;
+        }
+
+        public Message(Stream BodyStream)
+        {
+            initializeMessage();
+            this.BodyStream = BodyStream;
+        }
+
+        private void initializeMessage()
+        {
+            Body = null;
+            BodyStream = null;
+            //ContentType = null;
+            //CorrelationId = null;
+            //DeliveryCount = 0;
+            //EnqueuedSequenceNumber = 0;
+            //EnqueuedTimeUtc = new DateTime();
+            //ExpiresAtUtc = new DateTime();
+          
+            //IsBodyConsumed = false;
+            //Label = null;
+            //LockedUntilUtc = new DateTime();
+            //LockToken = new Guid();
+            MessageId = new Guid().ToString();
+            //PartitionKey = null;
+            Properties = new Dictionary<string, object>();
+            //ReplyTo = null;
+            //ReplyToSessionId = null;
+            //ScheduledEnqueueTimeUtc = new DateTime();
+            //SequenceNumber = 0;
+            //SessionId = null;
+            //Size = 0;
+           
+            //TimeToLive = new TimeSpan();
+            //To = null;
+            //ViaPartitionKey = null;
+
+        }
+        public T GetBody<T>()
+        {
+
+            if (this.Body != null)
+            {
+                return (T)Body;
+            }
+            else if (BodyStream != null && typeof(T) == typeof(Stream))
+            {
+                return (T)(object)BodyStream;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+    }
+}
