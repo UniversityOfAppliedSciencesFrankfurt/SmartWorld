8fee4831791f3f0ba49bdd4c35fc40434f116d14
Damir Dobric <ddobric@daenet.de>
2016-03-20 07:53:14 +0100

    unit tests addedd.

---
+++ Connectors/DeleteMe/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Azure.IotHubConnector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DeleteMe
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            run();
+        }
+
+        private void run()
+        {
+            IotHubConnector conn = new IotHubConnector();
+            conn.Open(new Dictionary<string, object>() {
+                { "ConnStr", "HostName=DRoth-IotHub-01.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=G0ddVsZb5UYFIt2iVTnN+psldF0qRHHxKMUcAo1tdWE=" },
+                { "DeviceId", "PI2-01" },
+            }).Wait();
+        }
+    }
+}
--- Connectors/IoTHubUnitTests/UnitTest1.cs
+++ Connectors/IoTHubUnitTests/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Azure.IotHubConnector;
+using System.Collections.Generic;
+using System.Configuration;
 
 namespace IoTHubUnitTests
 {
@@ -7,8 +10,52 @@ namespace IoTHubUnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InitWithExpliciteDeviceId_Test()
         {
+            IotHubConnector conn = new IotHubConnector();
+            conn.Open(new Dictionary<string, object>() {
+                { "ConnStr", ConfigurationManager.AppSettings["ConnStr"] },
+                { "DeviceId", ConfigurationManager.AppSettings["DeviceId"] },
+            }).Wait();
+
+            conn.SendAsync(
+                new { temperature = 22.0, sensor="unittest", messageId="1"},
+                (msgs) =>
+                {
+                    Assert.IsTrue(msgs.Count == 1);
+                    dynamic msg = msgs[0];
+                    Assert.IsTrue(msg.messageId == "1");
+                },
+
+                (msgs, err) =>
+                {
+                    throw err;
+                }).Wait();
+        }
+
+
+             [TestMethod]
+        public void InitWithDeviceIdInConnStr_Test()
+        {
+            string conStr = $"{ConfigurationManager.AppSettings["ConnStr"]};{ConfigurationManager.AppSettings["DeviceId"]}";
+            IotHubConnector conn = new IotHubConnector();
+            conn.Open(new Dictionary<string, object>() {
+                { "ConnStr", "DeviceId=PI2-01;HostName=DRoth-IotHub-01.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=G0ddVsZb5UYFIt2iVTnN+psldF0qRHHxKMUcAo1tdWE=" },
+            }).Wait();
+
+            conn.SendAsync(
+             new { temperature = 22.0, sensor = "unittest", messageId = "123" },
+             (msgs) =>
+             {
+                 Assert.IsTrue(msgs.Count == 1);
+                 dynamic msg = msgs[0];
+                 Assert.IsTrue(msg.messageId == "123");
+             },
+
+             (msgs, err) =>
+             {
+                 throw err;
+             }).Wait();
         }
     }
 }
