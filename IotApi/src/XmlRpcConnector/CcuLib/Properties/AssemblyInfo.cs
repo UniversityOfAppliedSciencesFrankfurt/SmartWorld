a411256bba0d8b2551b26fe01fad1ca2963b4118
Damir Dobric <ddobric@daenet.de>
2017-01-05 13:08:13 +0100

    refactoring nuget

--- IotApi/src/XmlRpcConnector/CcuLib/Properties/AssemblyInfo.cs
+++ IotApi/src/XmlRpcConnector/CcuLib/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CentranControlUnit")]
+[assembly: AssemblyProduct("CcuLib")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
