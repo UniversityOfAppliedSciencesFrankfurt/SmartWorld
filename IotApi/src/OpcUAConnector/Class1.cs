bf346eb52c35681af237a30085e17513286e4263
Syed <Syed@AQIB>
2017-03-25 01:31:07 +0100

    After Adding The Server and Client part of OPC UA

--- IotApi/src/OpcUAConnector/Class1.cs
+++ IotApi/src/OpcUAConnector/Class1.cs
@@ -2,13 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Iot;
 
 namespace OpcUAConnector
 {
-    public class Class1
+    public class Class1 : ISendModule, IReceiveModule
     {
         public Class1()
         {
+        }
+
+        public IReceiveModule NextReceiveModule
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ISendModule NextSendModule
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Open(Dictionary<string, object> args)
+        {
+            //TODO: 
+            //Connection part with client and server part 
+          namespace  Opc.Ua.Server;
+
+        }
+
+        public Task<object> ReceiveAsync(Dictionary<string, object> args = null)
+        {
+            //TODO: You have client now so receive part should be here
+        }
+
+        public Task ReceiveAsync(Action<IList<object>> onSuccess, Action<IList<object>, Exception> onError = null, Dictionary<string, object> args = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendAsync(IList<object> sensorMessages, Action<IList<object>> onSuccess = null, Action<IList<IotApiException>> onError = null, Dictionary<string, object> args = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendAsync(object sensorMessage, Action<object> onSuccess = null, Action<IotApiException> onError = null, Dictionary<string, object> args = null)
+        {
+           //TODO: You have client now so send part should be here
         }
     }
 }
