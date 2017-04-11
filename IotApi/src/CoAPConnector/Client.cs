834e14d3eca57d4aa37da22c84aecd25669332a5
Hien pham <mrhienpham001@gmail.com>
2017-04-11 16:02:23 +0200

    add message

--- IotApi/src/CoAPConnector/Client.cs
+++ IotApi/src/CoAPConnector/Client.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 
 namespace CoAPConnector
 {
-    public class Class1
+    public class Client
     {
-        public Class1()
+        public Client()
         {
-            Console.WriteLine("Test");
+            Console.WriteLine("Test2");
         }
     }
 }
