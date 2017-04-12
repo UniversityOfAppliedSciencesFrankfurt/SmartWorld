5170a14163cbf9233d3ae1cc7d1a14f03a097348
Hien pham <mrhienpham001@gmail.com>
2017-04-12 14:35:21 +0200

    Recover project

--- IotApi/src/CoAPConnector/Class1.cs
+++ IotApi/src/CoAPConnector/Class1.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 
 namespace CoAPConnector
 {
-    public class Client
+    public class Class1
     {
-        public Client()
+        public Class1()
         {
-            Console.WriteLine("Test2");
+            
         }
     }
 }
