b6f4e06c1eac900a8cdd603ed138068ea712766a
Mofaggol Hoshen <summoncse@gmail.com>
2017-12-14 16:52:58 +0100

    Add unittest project for XmlRpc

---
+++ IotApi/tests/XmlRpcConnectorTests/XmlRpcUnitTests/UnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XmlRpcUnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
