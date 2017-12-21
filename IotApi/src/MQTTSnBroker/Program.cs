f71fb93c24108118b2a91009aea3eb367f863461
Mofaggol Hoshen <summoncse@gmail.com>
2017-12-21 11:26:08 +0100

    Add MQTTSn broker solution

---
+++ IotApi/src/MQTTSnBroker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MQTTSnBroker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
