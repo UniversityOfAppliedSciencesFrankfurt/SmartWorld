4f6125abdfc17f2a30ba23189b5e804fcdfe525e
Mofaggol Hoshen <summoncse@gmail.com>
2018-02-14 12:50:33 +0100

    Remove required attribute from interface

--- IotApi/src/ProfinetConnector/IReadMessage.cs
+++ IotApi/src/ProfinetConnector/IReadMessage.cs
@@ -1,23 +1,15 @@
 ﻿using Dacs7.Domain;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ProfinetConnector
 {
     public interface IReadMessage
     {
-        [Required]
         PlcArea Area { get; set; }
-
-        [Required]
         int Offset { get; set; }
-
-        [Required]
         Type Type { get; set; }
-
-        [Required]
-        int[] Arg { get; set; }
+        int[] Args { get; set; }
     }
 }
--- IotApi/src/ProfinetConnector/ISensorMessage.cs
+++ IotApi/src/ProfinetConnector/ISensorMessage.cs
@@ -1,23 +1,15 @@
 ﻿using Dacs7.Domain;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ProfinetConnector
 {
     public interface ISensorMessage
     {
-        [Required]
         PlcArea Area { get; set; }
-
-        [Required]
         int Offset { get; set; }
-
-        [Required]
         object Value { get; set; }
-
-        [Required]
         int[] Args { get; set; }
     }
 }
